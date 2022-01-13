5c046943e0ffa0621779aa526ade0693aa3167d3
Ksenia Martynova <hurstcain@gmail.com>
2022-01-13 20:25:34 +0300

    Added UserSerializerCreateDetail and updated serializers for tv and movie

--- film_library/api/serializers.py
+++ film_library/api/serializers.py
@@ -1,27 +1,50 @@
 from rest_framework import serializers
-from film_library.api.models import *
 from django.contrib.auth.models import User
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'password', 'email', 'first_name', 'last_name', 'is_superuser', 'is_staff', 'is_active']
+from django.contrib.auth.hashers import make_password
+from film_library.api.models import *
 
 
 class UserSerializerList(serializers.HyperlinkedModelSerializer):
     tv_added = serializers.HyperlinkedRelatedField(many=True, view_name='tv-detail', read_only=True)
     movies_added = serializers.HyperlinkedRelatedField(many=True, view_name='movie-detail', read_only=True)
-    films_watched = serializers.HyperlinkedRelatedField(many=True, view_name='watched-detail', read_only=True)
+    films_watched = serializers.HyperlinkedRelatedField(many=True, view_name='user-watched-list', read_only=True)
     films_to_watch = serializers.HyperlinkedRelatedField(many=True, view_name='to-watch-detail', read_only=True)
+    id = serializers.HyperlinkedIdentityField(view_name='user-detail')
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'is_superuser', 'tv_added', 'movies_added',
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_superuser', 'tv_added', 'movies_added',
                   'films_watched', 'films_to_watch']
 
 
-class TVSerializer(serializers.ModelSerializer):
+class UserSerializerCreateDetail(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name', 'is_superuser',
+                  'is_staff', 'is_active']
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'required': True,
+                'style': {'input_type': 'password', 'placeholder': 'Password'}
+            }
+        }
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        return super(UserSerializerCreateDetail, self).create(validated_data)
+
+
+class TVSerializerList(serializers.HyperlinkedModelSerializer):
+    added_by = serializers.ReadOnlyField(source='added_by.username')
+    id = serializers.HyperlinkedIdentityField(view_name='tv-detail')
+
+    class Meta:
+        model = TV
+        fields = ['id', 'title', 'year', 'rating', 'genre', 'number_of_episodes', 'avg_episode_duration', 'added_by']
+
+
+class TVSerializerDetail(serializers.ModelSerializer):
     added_by = serializers.ReadOnlyField(source='added_by.username')
 
     class Meta:
@@ -29,7 +52,16 @@ class TVSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'year', 'rating', 'genre', 'number_of_episodes', 'avg_episode_duration', 'added_by']
 
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieSerializerList(serializers.HyperlinkedModelSerializer):
+    added_by = serializers.ReadOnlyField(source='added_by.username')
+    id = serializers.HyperlinkedIdentityField(view_name='movie-detail')
+
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'year', 'rating', 'genre', 'duration', 'added_by']
+
+
+class MovieSerializerDetail(serializers.ModelSerializer):
     added_by = serializers.ReadOnlyField(source='added_by.username')
 
     class Meta:
