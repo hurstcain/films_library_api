916c3e19c7eea842ffb30bcc38a7f599308ccc67
Ksenia Martynova <hurstcain@gmail.com>
2022-01-13 20:21:16 +0300

    Added IsCreatorOrReadOnly permission

--- film_library/api/permissions.py
+++ film_library/api/permissions.py
@@ -14,3 +14,11 @@ class IsSuperuserOrReadOnly(permissions.BasePermission):
             return True
         if User.objects.filter(username=request.user, is_superuser=True):
             return True
+
+
+class IsCreatorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if obj.user == request.user:
+            return True
