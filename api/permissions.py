37647dbb3ab695f98463904f6ef73c1cfd4d1668
Ksenia Martynova <hurstcain@gmail.com>
2022-01-19 19:44:39 +0300

    Added comments

--- api/permissions.py
+++ api/permissions.py
@@ -3,12 +3,21 @@ from django.contrib.auth.models import User
 
 
 class IsSuperuser(permissions.BasePermission):
+    """
+    Разрешение только для суперпользователей
+    """
+
     def has_permission(self, request, view):
         if User.objects.filter(username=request.user, is_superuser=True):
             return True
 
 
 class IsSuperuserOrReadOnly(permissions.BasePermission):
+    """
+    Разрешение для суперпользователей на чтение/запись.
+    Разрешение только для чтения, если не суперпользователь.
+    """
+
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
@@ -17,6 +26,11 @@ class IsSuperuserOrReadOnly(permissions.BasePermission):
 
 
 class IsCreatorOrReadOnly(permissions.BasePermission):
+    """
+    Разрешение для создателей на чтение/запись.
+    Разрешение только для чтения, если не создатель.
+    """
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
